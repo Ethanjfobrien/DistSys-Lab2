77a189ce588865263b7c6de791737535ebd07bf5
Ethan <basscase@hotmail.co.uk>
2015-02-20 22:39:39 +0000

    add cookie check to UserStyleChangeServlet

---
+++ Lab2/src/java/ADTs/User.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ADTs;
+
+/**
+ *
+ * @author ethan
+ */
+public class User {
+    public String textSize;
+    public String color;
+    public String bgColor;
+            
+}
